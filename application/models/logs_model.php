5ef964b3197a6842c3e36008f1a9c72f4f435055
Tyler <tdashton@gmail.com>
2016-09-06 23:11:35 +0200

    update to count the actual number of configured sensors, update interval in sql

--- application/models/logs_model.php
+++ application/models/logs_model.php
@@ -12,16 +12,14 @@ class Logs_model extends CI_Model {
 
     public function get_current($slug = FALSE)
     {
-        if ($slug === FALSE)
-        {
-
+        if ($slug === FALSE) {
             $this->db->select('value / 1000 as value, datetime, fk_sensor, description');
             $this->db->from('logs');
             $this->db->join('sensors', 'sensors.name = logs.fk_sensor');
-            $this->db->where('datetime > date_sub(now(), interval 1 minute)');
+            $this->db->where('datetime > date_sub(now(), interval 90 second)');
             $this->db->order_by('datetime', 'DESC');
             $this->db->order_by('fk_sensor', 'DESC');
-            $this->db->limit(2);
+            $this->db->limit(count($this->sensors));
             $query = $this->db->get();
             return $query->result_array();
         }
