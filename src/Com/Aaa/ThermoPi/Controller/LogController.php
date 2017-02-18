973c5db6f486d2eee14e1e86dbfedcca507286e5
Herr Tyler <tdashton@gmail.com>
2017-02-18 19:28:00 +0100

    added control / new routes

--- src/Com/Aaa/ThermoPi/Controller/LogController.php
+++ src/Com/Aaa/ThermoPi/Controller/LogController.php
@@ -7,19 +7,26 @@ namespace Com\Aaa\ThermoPi\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class LogController extends Controller
 {
 
     public function indexAction(Request $request)
     {
-
+        return new Response();
     }
 
     public function graphAction(Request $request)
     {
+        return new Response();
+    }
 
+    public function historyAction(Request $request)
+    {
+        return new JsonResponse();
     }
 
 }
