3c1d8f0346c484751a039441c9d9a8721b111b59
Herr Tyler <tdashton@gmail.com>
2017-11-20 21:20:16 +0100

    remove / rename incorrectly named model

--- application/models/control_logs_model.php
+++ application/models/control_logs_model.php
@@ -6,10 +6,37 @@ class Control_logs_model extends CI_Model {
         $this->load->database();
     }
 
+    /**
+     *
+     *
+     */
     public function insert_control_value($type, $param) {
         $this->db->set('datetime', 'now()', false);
         $this->db->replace('control_logs', 
             array('type' => $type, 'param' => $param));
         return;
+    }
+
+    /**
+     *
+     *
+     */
+    public function log_control_value($type, $param) {
+        $this->db->set('datetime', 'now()', false);
+        $this->db->insert('control_logs',
+            array('type' => $type, 'param' => $param));
+        return;
+    }
+
+    /**
+     * @return array an empty array if nothing was found
+     */
+    public function get_last_control_values()
+    {
+        $this->db->select('*');
+        $this->db->from('control_logs');
+        $this->db->where('datetime > date_sub(now(), interval 60 second)');
+        $query = $this->db->get();
+        return $query->result();
     }
 }
