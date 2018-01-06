8050657a0d25e8bc6ac26c26d11a5061362b574e
Herr Tyler <tdashton@gmail.com>
2018-01-06 19:35:45 +0100

    db agnostic continued, cleanup unused methods

--- application/models/Control_logs_model.php
+++ application/models/Control_logs_model.php
@@ -8,22 +8,10 @@ class Control_logs_model extends CI_Model {
 
     /**
      *
-     *
      */
     public function insert_control_value($type, $param) {
-        $this->db->set('datetime', 'now()', false);
+        $this->db->set('datetime', (new \DateTime())->format('Y-m-d H:i:s'));
         $this->db->replace('control_logs', 
-            array('type' => $type, 'param' => $param));
-        return;
-    }
-
-    /**
-     *
-     *
-     */
-    public function log_control_value($type, $param) {
-        $this->db->set('datetime', 'now()', false);
-        $this->db->insert('control_logs',
             array('type' => $type, 'param' => $param));
         return;
     }
@@ -35,20 +23,8 @@ class Control_logs_model extends CI_Model {
         $this->db->where('type = ', $type);
         $this->db->group_by('param');
         $this->db->order_by('countx desc');
-
         $query = $this->db->get();
-        return $query->result();
-    }
 
-    /**
-     * @return array an empty array if nothing was found
-     */
-    public function get_last_control_values()
-    {
-        $this->db->select('*');
-        $this->db->from('control_logs');
-        $this->db->where('datetime > date_sub(now(), interval 60 second)');
-        $query = $this->db->get();
         return $query->result();
     }
 }
